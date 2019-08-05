65253275ed3f834972c67e0e83b62d753b44e377
Benjamin Alan Weaver <baweaver@lbl.gov>
2019-08-05 14:40:47 -0700

    add version string

---
+++ py/desitransfer/__init__.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+============
+desitransfer
+============
+
+DESI_ data transfer infrastructure.
+
+.. _DESI: http://desi.lbl.gov
+"""
+#
+# Set version string.
+#
+from ._version import __version__
