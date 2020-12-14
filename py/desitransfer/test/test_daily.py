423dcd3e2f7012eca7b21f5bfd4c9c79d662b67a
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-12-14 12:06:37 -0700

    sync critical directories before shutdown

--- py/desitransfer/test/test_daily.py
+++ py/desitransfer/test/test_daily.py
@@ -103,6 +103,7 @@ class TestDaily(unittest.TestCase):
         mock_chmod.assert_has_calls([call('/dst/d0', 1512),
                                      call('/dst/d0/f1', 288),
                                      call('/dst/d0/f2', 288)])
+
     @patch('os.walk')
     @patch('os.chmod')
     def test_lock(self, mock_chmod, mock_walk):
