0beec3d79695ff684fb271c28ae9c6e8f4666011
Benjamin Alan Weaver <baweaver@lbl.gov>
2021-01-08 10:46:39 -0700

    allow for files in subdirectories

--- bin/desi_checksum_audit.sh
+++ bin/desi_checksum_audit.sh
@@ -7,10 +7,10 @@ data=${DESI_SPECTRO_DATA}
 for c in $(/usr/bin/find ${data} -name \*.sha256sum); do
     d=$(/usr/bin/dirname ${c})
     b=$(/usr/bin/basename ${c})
-    echo cd ${d}
+    echo "DEBUG: cd ${d}"
     cd ${d}
     n=$(/usr/bin/wc -l ${b} | /usr/bin/cut -d' ' -f1)
-    l=$(/usr/bin/ls -1 | /usr/bin/wc -l)
+    l=$(/usr/bin/find . -type f | /usr/bin/wc -l)
     if ((n == l - 1)); then
         echo "DEBUG: correct number of files listed in ${c}."
     elif ((n > l - 1)); then
@@ -18,6 +18,10 @@ for c in $(/usr/bin/find ${data} -name \*.sha256sum); do
     else
         echo "ERROR: extra files listed in ${c}!"
     fi
-    echo /usr/bin/sha256sum -c ${b}
-    /usr/bin/sha256sum -c ${b}
+    echo "DEBUG: /usr/bin/sha256sum --check --status ${b}"
+    /usr/bin/sha256sum --check --status ${b}
+    if [[ $? != 0 ]]; then
+        echo "ERROR: checksum error(s) reported for ${b}!"
+        /usr/bin/sha256sum --check ${b}
+    fi
 done
