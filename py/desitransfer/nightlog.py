59a5733f9687ced845d6847b56ad486109a003a1
Benjamin Alan Weaver <baweaver@lbl.gov>
2021-02-17 09:12:08 -0700

    add initial nightlog files

---
+++ py/desitransfer/nightlog.py
@@ -0,0 +1,233 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+desitransfer.nightlog
+=====================
+
+Bi-directional sync of KPNO and NERSC night.
+
+Run as a cron job on ``desi@dtn01.nersc.gov``.
+"""
+import logging
+import os
+import stat
+import subprocess as sub
+import time
+from argparse import ArgumentParser
+from logging.handlers import RotatingFileHandler, SMTPHandler
+from pkg_resources import resource_filename
+from socket import getfqdn
+from tempfile import TemporaryFile
+from desiutil.log import get_logger
+from .common import rsync, today
+# from .daemon import _popen
+from . import __version__ as dtVersion
+
+
+log = None
+
+
+def _options():
+    """Parse command-line options for :command:`desi_nightlog_transfer`.
+
+    Returns
+    -------
+    :class:`argparse.Namespace`
+        The parsed command-line options.
+    """
+    desc = "Transfer DESI nightlog data files."
+    prsr = ArgumentParser(description=desc)
+    # prsr.add_argument('-B', '--no-backup', action='store_false', dest='backup',
+    #                   help="Skip NERSC HPSS backups.")
+    # prsr.add_argument('-c', '--configuration', metavar='FILE',
+    #                   help="Read configuration from FILE.")
+    prsr.add_argument('-d', '--debug', action='store_true',
+                      help='Set log level to DEBUG.')
+    prsr.add_argument('-k', '--kill', metavar='FILE',
+                      default=os.path.join(os.environ['HOME'], 'stop_desi_transfer'),
+                      help="Exit the script when FILE is detected (default %(default)s).")
+    prsr.add_argument('-P', '--no-permission', action='store_false', dest='permission',
+                      help='Do not set permissions for DESI collaboration access.')
+    # prsr.add_argument('-S', '--shadow', action='store_true',
+    #                   help='Observe the actions of another data transfer script but do not make any changes.')
+    prsr.add_argument('-s', '--sleep', metavar='M', type=int, default=1,
+                      help='Sleep M minutes before checking for new data (default %(default)s minutes).')
+    prsr.add_argument('-V', '--version', action='version',
+                      version='%(prog)s {0}'.format(dtVersion))
+    return prsr.parse_args()
+
+
+def _configure_log(debug):
+    """Re-configure the default logger returned by ``desiutil.log``.
+
+    Parameters
+    ----------
+    debug : :class:`bool`
+        If ``True`` set the log level to ``DEBUG``.
+    """
+    global log
+    # conf = self.conf['logging']
+    log = get_logger(timestamp=True)
+    h = log.parent.handlers[0]
+    handler = RotatingFileHandler(os.path.join(os.environ['DESI_ROOT'], 'spectro', 'nightlog', 'desi_nightlog_transfer.log'),
+                                  maxBytes=100000000,
+                                  backupCount=100)
+    handler.setFormatter(h.formatter)
+    log.parent.removeHandler(h)
+    log.parent.addHandler(handler)
+    if debug:
+        log.setLevel(logging.DEBUG)
+    email_from = os.environ['USER'] + '@' + getfqdn()
+    handler2 = SMTPHandler('localhost', email_from, ['desi-alarms-transfer@desi.lbl.gov', ],
+                           'Critical error reported by desi_nightlog_transfer!')
+    fmt = """Greetings,
+
+At %(asctime)s, desi_nightlog_transfer failed with this message:
+
+%(message)s
+
+Kia ora koutou,
+The DESI Collaboration Account
+"""
+    formatter2 = logging.Formatter(fmt, datefmt='%Y-%m-%d %H:%M:%S %Z')
+    handler2.setFormatter(formatter2)
+    handler2.setLevel(logging.CRITICAL)
+    log.parent.addHandler(handler2)
+
+
+def _popen(command):
+    """Simple wrapper for :class:`subprocess.Popen` to avoid repeated code.
+
+    Parameters
+    ----------
+    command : :class:`list`
+        Command to pass to :class:`subprocess.Popen`.
+
+    Returns
+    -------
+    :func:`tuple`
+        The returncode, standard output and standard error.
+    """
+    log.debug(' '.join(command))
+    with TemporaryFile() as tout, TemporaryFile() as terr:
+        p = sub.Popen(command, stdout=tout, stderr=terr)
+        p.wait()
+        tout.seek(0)
+        out = tout.read()
+        terr.seek(0)
+        err = terr.read()
+    return (str(p.returncode), out.decode('utf-8'), err.decode('utf-8'))
+
+
+def main():
+    """Entry point for :command:`desi_nightlog_transfer`.
+
+    Returns
+    -------
+    :class:`int`
+        An integer suitable for passing to :func:`sys.exit`.
+    """
+    options = _options()
+    _configure_log(options.debug)
+    errcount = 0
+    wait = options.sleep*60
+    source = '/exposures/nightlog'
+    basedir = os.path.join(os.environ['DESI_ROOT'], 'spectro', 'nightlog')
+    kpnodir = os.path.join(basedir, 'kpno')
+    # syncdir = os.path.join(basedir, 'sync')
+    exclude = resource_filename('desitransfer', 'data/desi_nightlog_transfer_exclude.txt')
+    include = resource_filename('desitransfer', 'data/desi_nightlog_transfer_include.txt')
+    with open(include) as i:
+        top_level_files = i.read().strip().split('\n')
+    log.debug(', '.join(top_level_files))
+    top_level_files_mode = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH
+    while True:
+        log.info('Starting nightlog transfer loop; desitransfer version = %s.',
+                 dtVersion)
+        if os.path.exists(options.kill):
+            log.info("%s detected, shutting down nightlog daemon.",
+                     options.kill)
+            return 0
+        night = today()
+        t0 = time.time()
+        #
+        # First check if there is any data for tonight yet.
+        #
+        log.info('Checking for nightlog data from %s.', night)
+        cmd = ['/bin/rsync', 'dts:{0}/'.format(source)]
+        status, out, err = _popen(cmd)
+        found = False
+        if status != '0':
+            errcount += 1
+            log.error('Getting file list for %s; trying again in %d minutes.', night, options.sleep)
+            time.sleep(wait)
+            continue
+        for line in out.split('\n'):
+            if line.endswith(night):
+                log.info(line)
+                found = True
+        if not found:
+            log.info('No nightlog data found for %s; trying again in %d minutes.', night, options.sleep)
+            time.sleep(wait)
+            continue
+        #
+        # Sync per-night directory.
+        #
+        nightdir = os.path.join(kpnodir, night)
+        cmd = rsync(os.path.join(source, night), nightdir)
+        cmd.insert(cmd.index('--omit-dir-times') + 1, '--exclude-from')
+        cmd.insert(cmd.index('--exclude-from') + 1, exclude)
+        log.info('Syncing %s.', night)
+        status, out, err = _popen(cmd)
+        if status != '0':
+            errcount += 1
+            log.error('Syncing %s.', night)
+        #
+        # Correct the permissions.
+        #
+        if options.permission:
+            if os.path.exists(nightdir):
+                log.info('Fixing permissions for DESI.')
+                cmd = ['fix_permissions.sh', nightdir]
+                status, out, err = _popen(cmd)
+                if status != '0':
+                    errcount += 1
+                    log.error('Fixing permissions for %s.', nightdir)
+            else:
+                log.info('No data yet for night %s.', night)
+        else:
+            log.info("Skipping permission changes at user request.")
+        #
+        # Sync the top level files; skip the logs.
+        #
+        log.info('Syncing top level html/js files.')
+        cmd = ['/bin/rsync', '--verbose', '--links', '--times', '--files-from',
+               include,
+               'dts:{0}/'.format(source),
+               '{0}/'.format(kpnodir)]
+        status, out, err = _popen(cmd)
+        if status != '0':
+            errcount += 1
+            log.error('Syncing top level html files.')
+        #
+        # Hack: just add world read to those top level files since fix_permissions.sh
+        # is recursive and we don't want to redo all nights.
+        #
+        for filename in top_level_files:
+            log.debug("os.chmod('%s', 0o%o)",
+                      os.path.join(kpnodir, filename),
+                      top_level_files_mode)
+            os.chmod(os.path.join(kpnodir, filename), top_level_files_mode)
+        #
+        # Check for accumulated errors.
+        #
+        if errcount > 10:
+            log.critical('Transfer error count exceeded, shutting down.')
+            return 1
+        #
+        # If all that took less than 10 minutes, sleep a bit.
+        #
+        dt = time.time() - t0
+        if dt < wait:
+            log.info('Sleeping for a bit.')
+            time.sleep(wait - dt)
---
+++ py/desitransfer/test/test_nightlog.py
@@ -0,0 +1,89 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""Test desitransfer.nightlog.
+"""
+# import datetime
+import logging
+import os
+# import shutil
+import sys
+import unittest
+from tempfile import TemporaryDirectory
+from unittest.mock import call, patch, MagicMock
+from pkg_resources import resource_filename
+from ..nightlog import (_options, _popen, _configure_log)
+
+
+class TestNightlog(unittest.TestCase):
+    """Test desitransfer.nightlog.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        """Create a temporary directory to simulate CSCRATCH.
+        """
+        self.tmp = TemporaryDirectory()
+
+    def tearDown(self):
+        """Clean up temporary directory.
+        """
+        self.tmp.cleanup()
+
+    def test_options(self):
+        """Test command-line arguments.
+        """
+        with patch.object(sys, 'argv', ['desi_nightlog_transfer', '--debug']):
+            options = _options()
+            self.assertTrue(options.debug)
+            self.assertEqual(options.kill,
+                             os.path.join(os.environ['HOME'],
+                                          'stop_desi_transfer'))
+
+    @patch('desitransfer.nightlog.SMTPHandler')
+    @patch('desitransfer.nightlog.RotatingFileHandler')
+    @patch('desitransfer.nightlog.get_logger')
+    @patch('desitransfer.nightlog.log')  # Needed to restore the module-level log object after test.
+    def test_configure_log(self, mock_log, gl, rfh, smtp):
+        """Test logging configuration.
+        """
+        with patch.dict('os.environ',
+                        {'CSCRATCH': self.tmp.name,
+                         'DESI_ROOT': '/desi/root',
+                         'DESI_SPECTRO_DATA': '/desi/root/spectro/data'}):
+            with patch.object(sys, 'argv', ['desi_nightlog_transfer', '--debug']):
+                options = _options()
+            _configure_log(options)
+        rfh.assert_called_once_with('/desi/root/spectro/nightlog/desi_nightlog_transfer.log',
+                                    backupCount=100, maxBytes=100000000)
+        gl.assert_called_once_with(timestamp=True)
+        gl().setLevel.assert_called_once_with(logging.DEBUG)
+
+    @patch('desitransfer.nightlog.TemporaryFile')
+    @patch('subprocess.Popen')
+    @patch('desitransfer.nightlog.log')
+    def test_popen(self, mock_log, mock_popen, mock_temp):
+        """Test Popen wrapper.
+        """
+        mock_file = mock_temp().__enter__.return_value = MagicMock()
+        mock_file.read.return_value = b'MOCK'
+        proc = mock_popen.return_value = MagicMock()
+        proc.returncode = 0
+        pp = _popen(['foo', 'bar'])
+        self.assertEqual(pp, ('0', 'MOCK', 'MOCK'))
+        mock_log.debug.assert_called_once_with('foo bar')
+        mock_popen.assert_called_once_with(['foo', 'bar'], stdout=mock_file, stderr=mock_file)
+
+
+def test_suite():
+    """Allows testing of only this module with the command::
+
+        python setup.py test -m <modulename>
+    """
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
