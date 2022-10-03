47f564625ffd25a850e04e020ecf2b9a58b3a77b
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2022-10-03 11:27:48 -0700

    fix syntax

--- bin/desi_transfer_statistics.sh
+++ bin/desi_transfer_statistics.sh
@@ -2,6 +2,8 @@
 #
 # Collect (meta) data on files transferred for annual reporting purposes.
 #
+# Total amount of raw data.
+#
 year=$1
 next_year=$(( year + 1 ))
 # fy=${year}1[012][0-9][0-9] ${next_year}0[0-9][0-9][0-9]
@@ -16,7 +18,7 @@ for n in ${DESI_SPECTRO_DATA}/${year}1[012][0-9][0-9] ${DESI_SPECTRO_DATA}/${nex
         echo ${e}
         expid=$(basename ${e})
         number_of_exposures=$(( number_of_exposures + 1 ))
-        expid_size=$(du -k -s ${e})
+        expid_size=$(du -k -s ${e} | awk '{print $1}')
         total_data=$(( total_data + expid_size ))
     done
 done
@@ -37,7 +39,7 @@ if [[ "${year}" == "2021" ]]; then
         for e in ${DESI_SPECTRO_DATA}/${night}/*; do
             echo ${e}
             expid=$(basename ${e})
-            expid_size=$(du -k -s ${e})
+            expid_size=$(du -k -s ${e} | awk '{print $1}')
             number_of_fire_exposures=$(( number_of_fore_exposures + 1 ))
             total_data=$(( total_fire_data + expid_size ))
     done
