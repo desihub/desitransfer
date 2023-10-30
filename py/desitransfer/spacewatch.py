a6a00350d0caf1b7abb8a09a6c5c6b35713a5af8
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2023-10-30 16:40:48 -0700

    placeholder for spacewatch

---
+++ py/desitransfer/spacewatch.py
@@ -0,0 +1,19 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+desitransfer.spacewatch
+=======================
+
+Entry point for :command:`desi_spacewatch_transfer`.
+"""
+
+
+def main():
+    """Entry point for :command:`desi_spacewatch_transfer`.
+
+    Returns
+    -------
+    :class:`int`
+        An integer suitable for passing to :func:`sys.exit`.
+    """
+    return 0
